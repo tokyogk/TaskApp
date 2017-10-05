22e78c915e1085b19b4104503b8f2136bec95c78
tokyogk <tokyo.gk5@gmail.com>
2017-10-05 18:32:29 +0900

    途中経過10/05 serchcateというメゾットを作りました。

--- app/src/main/java/jp/techacademy/gou/kuwabara/taskapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/gou/kuwabara/taskapp/MainActivity.java
@@ -139,7 +139,8 @@ public class MainActivity extends AppCompatActivity {
     private void addTaskForTest() {
         Task task = new Task();
         task.setTitle("作業");
-       // task.setCategory("カテゴリー");
+        task.setCategory("カテゴリー");
+        task.setSerchCate("カテサーチ");
         task.setContents("プログラムを書いてPUSHする");
         task.setDate(new Date());
         task.setId(0);
